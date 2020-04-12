0bc563fce25bdad3b4224a1a82e604ca7c45e7f7
Азиз Исматов <yourGitHub@example.co>
2020-04-12 21:34:02 +0500

    Добавил объект Client и заполнил его поля

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace PL
         
         static void Main(string[] args)
         {
-          
+          string[]  name={"Алишер","Бахтиёр","Абубакр","Фируза"};
+          string[]  surname={"Аброров","Акрамов","Анваров","Усмонова"};
+          string[] Middlename={"Довудович","Ахмадович","Джамшедович","Олимович"};
+          string[] pol={"муж","муж","муж","жен"};
+          int[] nomerTelefon = new int[4]{918123456,907907907,908111111,909909909};
+          string[] adress = {"г.Кулоб ул Борбад дом 214","г Бохтар ул Маърифат дом 56","р.Рудаки городок Сомониён ул С.Давлатов дом 89/27","г.Душанбе Алишер Навои дом 78/56"};
+          for(int i=0;i<4;i++)
+          {
+
+           Abonent  client = new Abonent (surname[i],name[i],Middlename[i],nomerTelefon[i],adress[i],pol[i]);
+           client.getinfo(i+1);
+          }
         }
     
     }
